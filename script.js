fed248b3c7a62c006173149b48d106b7cbafa44a
Joseph Jayanth <samueljayanth@gmail.com>
2025-07-16 10:52:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fade-in animation on scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
